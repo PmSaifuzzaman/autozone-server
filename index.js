77942098448c17d47a2a997ab2c7388434fe5057
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-10-18 01:58:23 +0600

    Post and get api created for brands category

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -6,6 +7,52 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+// autoZone
+// DikwR9QqKOBNA2Lc
+
+const uri = "mongodb+srv://autoZone:DikwR9QqKOBNA2Lc@cluster0.ublbqgg.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const database = client.db("brandsDB");
+    const brandCollection = database.collection("brand");
+
+    app.get('/brands', async(req, res) => {
+        const cursor = brandCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.post('/brands', async(req, res) =>{
+        const brands = req.body;
+        console.log(brands)
+        const result = await brandCollection.insertOne(brands);
+        res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
